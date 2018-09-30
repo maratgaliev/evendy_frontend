fce6fd21a98941ded07c471eee97eb5f65f68ca1
kazanlug@gmail.com <kazanlug@gmail.com>
2018-09-30 22:30:13 +0300

    Use https

--- src/backend/vue-axios/axios.js
+++ src/backend/vue-axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.API_URL || 'http://api.evendy.ru'
+const API_URL = process.env.API_URL || 'https://api.evendy.ru'
 
 export default axios.create({
   baseURL: API_URL,
